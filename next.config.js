6edc20c47f2eac07015f88d578613595a35e8187
David Fernández <davidfernandezcomesana@gmail.com>
2023-12-26 21:03:32 +0100

    bugg arreglado

--- next.config.js
+++ next.config.js
@@ -19,7 +19,7 @@ module.exports = withStoreConfig({
       {
         protocol: "https",
         hostname: "medusa-alma-ecommerce.s3.eu-west-3.amazonaws.com",
-      }
+      },
       {
         protocol: "https",
         hostname: "medusa-public-images.s3.eu-west-1.amazonaws.com",
