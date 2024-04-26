75e0fde73c98ff6aca9e0ca057bab00217b1f33e
David Fernández <davidfernandezcomesana@gmail.com>
2024-04-26 11:54:01 +0200

    first commit

--- src/modules/home/components/hero/index.tsx
+++ src/modules/home/components/hero/index.tsx
@@ -1,33 +1,28 @@
-import { Github } from "@medusajs/icons"
-import { Button, Heading } from "@medusajs/ui"
+import { Heading } from "@medusajs/ui"
 
 const Hero = () => {
   return (
     <div className="h-[75vh] w-full border-b border-ui-border-base relative bg-ui-bg-subtle">
+      <img src="https://i.postimg.cc/Rh7c0gtS/fondo-test.webp" alt="fondo" className="w-[100%] absolute h-[100%]"/>
       <div className="absolute inset-0 z-10 flex flex-col justify-center items-center text-center small:p-32 gap-6">
-        <span>
+        <span className="flex flex-col gap-2">
           <Heading
             level="h1"
-            className="text-3xl leading-10 text-ui-fg-base font-normal"
+            className="text-[4rem] leading-10 text-ui-fg-base font-bold text-red-500"
           >
-            Ecommerce Starter Template
+            Birmin Gallery
           </Heading>
           <Heading
             level="h2"
-            className="text-3xl leading-10 text-ui-fg-subtle font-normal"
+            className="text-xl leading-10 text-ui-fg-subtle font-normal text-black"
           >
-            Powered by Medusa and Next.js
+            Store Shop
           </Heading>
         </span>
         <a
           href="https://github.com/medusajs/nextjs-starter-medusa"
           target="_blank"
-        >
-          <Button variant="secondary">
-            View on GitHub
-            <Github />
-          </Button>
-        </a>
+        ></a>
       </div>
     </div>
   )
